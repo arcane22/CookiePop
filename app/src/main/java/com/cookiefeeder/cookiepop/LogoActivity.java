05225ccdba68d2bdc24e9b026241d9f7b4a70907
hong3883 <hong3883@naver.com>
2020-07-10 15:30:18 +0900

    Add Logo Activity

---
+++ app/src/main/java/com/cookiefeeder/cookiepop/LogoActivity.java
@@ -0,0 +1,34 @@
+package com.cookiefeeder.cookiepop;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class LogoActivity extends AppCompatActivity
+{
+    private int timerCount;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_logo);
+        final Timer mTimer = new Timer();
+        TimerTask mTimerTask = new TimerTask()
+        {
+            @Override
+            public void run()
+            {
+                mTimer.cancel();
+                Intent intent = new Intent(getApplication(), LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        };
+        mTimer.schedule(mTimerTask, 2500);
+    }
+}
