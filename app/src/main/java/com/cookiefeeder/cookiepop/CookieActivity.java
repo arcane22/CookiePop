2485d70db58de64eb853b896f892a8a9d90af40b
jeehoonK-coder <64194416+jeehoonK-coder@users.noreply.github.com>
2020-08-20 21:43:01 +0900

    액티비티 애니메이션 및 기타 UI작업

--- app/src/main/java/com/cookiefeeder/cookiepop/CookieActivity.java
+++ app/src/main/java/com/cookiefeeder/cookiepop/CookieActivity.java
@@ -4,14 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
-import android.widget.CalendarView;
-import android.widget.EditText;
 import android.widget.NumberPicker;
 import android.widget.TextView;
 import android.widget.TimePicker;
--- app/src/main/java/com/cookiefeeder/cookiepop/HomeFragment.java
+++ app/src/main/java/com/cookiefeeder/cookiepop/HomeFragment.java
@@ -1,17 +1,31 @@
 package com.cookiefeeder.cookiepop;
 
+import android.animation.ObjectAnimator;
 import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
+import android.widget.FrameLayout;
+import android.widget.Toast;
 
-public class HomeFragment extends Fragment
-{
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+public class HomeFragment extends Fragment implements View.OnClickListener {
+
+    private Animation fab_open, fab_close, fab_small, fab_big;
+    private Boolean isFabOpen = false;
+    private FloatingActionButton mainFab;
+    private Button button1,button2,button3,button4,button5,button6;
+    private FrameLayout btn_layout;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
     {
@@ -44,12 +58,21 @@ public class HomeFragment extends Fragment
                 startActivity(intent);
             }
         };
-        Button button1 = (Button) root.findViewById(R.id.cookie1);
-        Button button2 = (Button) root.findViewById(R.id.cookie2);
-        Button button3 = (Button) root.findViewById(R.id.cookie3);
-        Button button4 = (Button) root.findViewById(R.id.cookie4);
-        Button button5 = (Button) root.findViewById(R.id.cookie5);
-        Button button6 = (Button) root.findViewById(R.id.cookie6);
+        fab_open = AnimationUtils.loadAnimation(getActivity(), R.anim.fab_open);
+        fab_close = AnimationUtils.loadAnimation(getActivity(), R.anim.fab_close);
+        fab_small = AnimationUtils.loadAnimation(getActivity(), R.anim.fab_small);
+        fab_big = AnimationUtils.loadAnimation(getActivity(), R.anim.fab_big);
+        btn_layout = root.findViewById(R.id.btn_layout);
+        mainFab = (FloatingActionButton) root.findViewById(R.id.fab);
+        mainFab.setOnClickListener(this);
+
+        button1 = (Button) root.findViewById(R.id.cookie1);
+        button2 = (Button) root.findViewById(R.id.cookie2);
+        button3 = (Button) root.findViewById(R.id.cookie3);
+        button4 = (Button) root.findViewById(R.id.cookie4);
+        button5 = (Button) root.findViewById(R.id.cookie5);
+        button6 = (Button) root.findViewById(R.id.cookie6);
+
         button1.setOnClickListener(listener);
         button2.setOnClickListener(listener);
         button3.setOnClickListener(listener);
@@ -57,5 +80,48 @@ public class HomeFragment extends Fragment
         button5.setOnClickListener(listener);
         button6.setOnClickListener(listener);
         return root;
+    }
+
+    @Override
+    public void onClick(View v) {
+        anim();
+
+
+    }
+    public void anim() {
+
+        if (isFabOpen) {
+            btn_layout.startAnimation(fab_big);
+            //mainFab.startAnimation(fab_big);
+            button1.startAnimation(fab_close);
+            button2.startAnimation(fab_close);
+            button3.startAnimation(fab_close);
+            button4.startAnimation(fab_close);
+            button5.startAnimation(fab_close);
+            button6.startAnimation(fab_close);
+            button1.setClickable(false);
+            button2.setClickable(false);
+            button3.setClickable(false);
+            button4.setClickable(false);
+            button5.setClickable(false);
+            button6.setClickable(false);
+            isFabOpen = false;
+        } else {
+            btn_layout.startAnimation(fab_small);
+            //mainFab.startAnimation(fab_small);
+            button1.startAnimation(fab_open);
+            button2.startAnimation(fab_open);
+            button3.startAnimation(fab_open);
+            button4.startAnimation(fab_open);
+            button5.startAnimation(fab_open);
+            button6.startAnimation(fab_open);
+            button1.setClickable(true);
+            button2.setClickable(true);
+            button3.setClickable(true);
+            button4.setClickable(true);
+            button5.setClickable(true);
+            button6.setClickable(true);
+            isFabOpen = true;
+        }
     }
 }
